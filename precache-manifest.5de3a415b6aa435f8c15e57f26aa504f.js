687d8a9a761336aea7664e9a57e264f5dadc8b13
Colin McCulloch <colinmucculloch@gmail.com>
2020-04-02 12:30:28 -0400

    Updates

---
+++ precache-manifest.5de3a415b6aa435f8c15e57f26aa504f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8b1b6c50efe7dd6ca3e0e9073981e9",
+    "url": "/slide-puzzle/index.html"
+  },
+  {
+    "revision": "106b1611086a6d3efc5b",
+    "url": "/slide-puzzle/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f480e2c7ef355c73c20",
+    "url": "/slide-puzzle/static/js/2.6197d411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slide-puzzle/static/js/2.6197d411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106b1611086a6d3efc5b",
+    "url": "/slide-puzzle/static/js/main.45457f7e.chunk.js"
+  },
+  {
+    "revision": "39eddb7b4a5956edc5b2",
+    "url": "/slide-puzzle/static/js/runtime-main.96367c88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slide-puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
