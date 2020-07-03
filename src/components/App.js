fa110b684475b6d7e568dcc0aa6d93bea5df5537
Colin McCulloch <colinmucculloch@gmail.com>
2020-07-03 09:19:15 -0400

    README.md

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Gameboard from './gameboard.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 className='col-12 text-center display-3'>Slide-Puzzle</h1>
+        <div className='row'>
+          <Gameboard rows={4} columns={4} />
+        </div>
+      </>
+    )
+  }
+}
---
+++ src/components/tile.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css';
+
+
+export default class Tile extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+
+        const num = this.props.index
+        const col = this.props.col
+        const row = this.props.row
+        const actualLoc = { row, col }
+        let x =num % 4
+        let y = Math.floor(num/this.props.width)
+        const imgY = y * -100
+        const imgX = x * -100
+        const myStyle = {
+            marginTop: imgY,
+            marginLeft: imgX
+        }
+        return (
+            <div
+                id={`${'tile' + this.props.index}`}
+                className='border col-3 tile'
+                
+                onClick={() => this.props.onClick(this.props.index, actualLoc)}
+            >
+                <img style={myStyle} className='img' src={`${this.props.picture === null ? process.env.PUBLIC_URL + '/dale.svg' : this.props.picture}`}
+                ></img>
+                {this.props.index}
+            </div>
+        )
+    }
+}
